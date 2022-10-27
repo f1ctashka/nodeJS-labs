95af071348995a0c478c7c15dd309c39a8a36623
Vadym <vadym.stroyy3@gmail.com>
2022-10-27 22:29:19 +0300

    feature: simple http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { createServer } from 'node:http';
+
+const listener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+
+  response
+    .writeHead(200)
+    .end(
+      '<img src="https://i.imgur.com/EDA6NLa.jpg" alt="Samoyed" width="200" />'
+    );
+};
+
+const server = createServer(listener);
+server.listen(3000);
